fa2393c49777adf00e80a70ba81901d1138fd74f
Arjun-glitch66 <arjunanand206@gmail.com>
2026-03-20 04:58:40 +0530

    switch to brevo email

--- server.js
+++ server.js
@@ -37,15 +37,12 @@ db.connect((err) => {
 
 // ✅ Email Setup (ENV based)
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp-relay.brevo.com",
     port: 587,
-    secure: false, // IMPORTANT
+    secure: false,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
+        user: "process.env.EMAIL_USER",
+        pass: "process.env.EMAIL_PASS"
     }
 });
 
